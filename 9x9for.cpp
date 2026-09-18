e294ad930b7705b9f17346aa12063a3f1994cc9c
wanhji <3611830443@qq.com>
2026-09-18 19:14:27 +0800

    Update 9x9for.cpp

--- 9x9for.cpp
+++ 9x9for.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main(){
     for(int y=1;y<=9;y++){
         for(int a=1;a<=y;a++){
+            if(a*y<10){
+            cout<<a<<"x"<<y<<"="<<a*y<<"  ";  
+            }
+            else
             cout<<a<<"x"<<y<<"="<<a*y<<" ";
             
 
